8fa1289b8ac477327a9c938448d9d462c7dd6ae1
NATILEMMA <natilemma5@gmail.com>
2022-08-10 10:34:21 +0300

    human resource web - app

---
+++ Bot/bot.js
@@ -0,0 +1,14 @@
+const { Telegraf } = require("telegraf");
+const TOKEN = "5580082490:AAFs1JQZ0iPtTnB8zDjil1E-00yaM64pKPg";
+const bot = new Telegraf(TOKEN);
+
+const web_link = "https://celebrated-torte-184681.netlify.app/";
+
+bot.start((ctx) =>
+  ctx.reply("Welcome :)))))", {
+    reply_markup: {
+      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+    },
+  })
+);
+bot.launch();
